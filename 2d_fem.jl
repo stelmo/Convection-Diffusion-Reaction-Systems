ff3006932ab2672298f2ebaf87f4eb1c8f8e8584
St Elmo <stelmozors@gmail.com>
2014-11-12 22:36:16 +0200

    started 2D FEM steady

---
+++ 2d_fem.jl
@@ -0,0 +1,17 @@
+# Two dimensional problem
+
+
+
+
+
+dx = 1.0
+dy = 1.0
+
+# M matrix - its symmetric
+m11 = (4./36.)*dx*dy
+m12 = (2./36.)*dx*dy
+m13 = (1./36.)*dx*dy
+m14 = (2./36.)*dx*dy
+m23 = (2./36.)*dx*dy
+m24 = (1./36.)*dx*dy
+m34 = (2./36.)*dx*dy
