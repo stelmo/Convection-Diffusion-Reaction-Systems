c29cc07557a898e71c347d6ac0c74b0ce2f00ddb
St Elmo <stelmozors@gmail.com>
2014-11-21 11:58:40 +0200

    working unsteady non-linear 1D fem

--- adi_1d_us.jl
+++ adi_1d_us.jl
@@ -4,18 +4,19 @@ using PyPlot
 a = 7.5 # Peclet number
 b = 5.129 #  L/U
 
-N = 10 # number of nodes - need many of these
+N = 100 # number of nodes - need many of these
 xs = linspace(0., 1., N)
 h = xs[2] - xs[1]
 
-Nt = 5
+Nt = 50
 ts = linspace(0., 2., Nt) # this is dimensionless time... It also takes long...
 dt = ts[2] - ts[1]
 
 us = zeros(N, Nt)
 
-# Linearised functions
-kT(x) = -1.75*x + 2.
+# Rate functions
+# kT(x) = 0.25
+# kT(x) = -1.75*x + 2.
 # kT(x) = -3.*x+3.2
 # kT(x) = (3.)*x.^2 -6.*x + 3.2
 # kT(x) = 1633253.0*exp(-4881.*(1./(300.+100.*(1.-x)))) # should be 2 910 122.0
@@ -60,80 +61,82 @@ K1 = K1[:, 2:end]
 K2 = K2[:, 2:end]
 
 # Start time loop
-for tloop=1:Nt
-  # Guess a solution
-  u0 = us[:, tloop]
-  u0[1] = 1.
-  for i=2:N
-    u0[i] =u0[i-1]*0.8
-  end
-  #
-    #
-  #
-  # newtonFlag = true
-  # counter = 0
-  # alpha = 0.8 # slow down NR - dampening
-  # maxIter = 1000
-  #
-  # while newtonFlag
-  #
-  #   # Assemble the non-linear non-constant matrices
-  #   # Non-linear reaction matrices
-  #   dm3 = fill(2.*h/3., N)
-  #   dm3[1] = h/3.
-  #   dm3[end] = h/3.
-  #
-  #   dl3 = fill(h/6., N-1)
-  #   du3 = fill(h/6., N-1)
-  #
-  #   K5 = diagm(dl3,-1) + diagm(dm3, 0) + diagm(du3, 1)
-  #   K5 = K5[2:end, :]
-  #   K6 = copy(K5)
-  #   K6 = a*b*K6
-  #   K5 = -a*b*K5
-  #
-  #   for row=1:N-1
-  #     K6[row, :] = kT(u0[row])*K6[row, :]
-  #     K5[row, :] = kT(u0[row])*K5[row, :]
-  #   end
-  #
-  #   dl4 = zeros(N-1)
-  #   for i=1:N-1
-  #     dl4[i] = kTd(u0[i+1])*(h/12.)*(u0[i]+u0[i+1])
-  #   end
-  #
-  #   du4 = zeros(N-1)
-  #   for i=1:N-1
-  #     du4[i] = kTd(u0[i])*(h/12.)*(u0[i]+u0[i+1])
-  #   end
-  #
-  #   dm4 = zeros(N)
-  #   dm4[1] = -9. # it gets cut away so w/e
-  #   for i=2:N-1
-  #     dm4[i] = kTd(u0[i])*(u0[i-1]*h/12. + u0[i]*h/2. + u0[i+1]*h/12.)
-  #   end
-  #   dm4[end] = kTd(u0[end])*(u0[end-1]*h/12. + u0[end]*h/4.)
-  #
-  #   K7 = diagm(dl4,-1) + diagm(dm4, 0) + diagm(du4, 1)
-  #   K7 = K7[2:end, :]
-  #   K7 = -a*b*K7
-  #
-  #   B1 = (K3 + K4 + K6)*u0
-  #
-  #   K5 = K5[:, 2:end]
-  #   K7 = K7[:, 2:end]
-  #   # du0 = 0 because we know u0 = 1 = u
-  #
-  #   KK = K1 + K2 + K5 + K7
-  #   du = \(KK, B1)
-  #   u0 = u0 + alpha*[0., du]
-  #
-  #   (abs(maximum(du)) < 1.E-05 || counter > maxIter) ? newtonFlag = false : newtonFlag = true
-  #   counter += 1
-  # end
+for tloop=2:Nt
 
-# plot(xs, u0)
-# xlabel("Dimensionless Axial Distance")
-# ylabel("Dimensionless Concentration")
-# title("Steady State Non-linear")
-# plt.show()
+  newtonFlag = true
+  counter = 0
+  alpha = 0.8 # slow down NR - dampening
+  maxIter = 1000
+
+  # Guess a solution
+  uguess = us[:, tloop-1] # guess the previous solution
+
+    while newtonFlag
+
+    # Assemble the non-linear non-constant matrices
+    dm3 = fill(2.*h/3., N)
+    dm3[1] = h/3.
+    dm3[end] = h/3.
+
+    dl3 = fill(h/6., N-1)
+    du3 = fill(h/6., N-1)
+
+    K5 = diagm(dl3,-1) + diagm(dm3, 0) + diagm(du3, 1)
+    K5 = K5[2:end, :]
+    K6 = copy(K5)
+    M = copy(K5)
+    M = a*M
+    K6 = a*b*K6
+    K5 = -a*b*K5
+
+    for row=1:N-1
+      K6[row, :] = kT(uguess[row])*K6[row, :]
+      K5[row, :] = kT(uguess[row])*K5[row, :]
+    end
+
+    dl4 = zeros(N-1)
+    for i=1:N-1
+      dl4[i] = kTd(uguess[i+1])*(h/12.)*(uguess[i]+uguess[i+1])
+    end
+
+    du4 = zeros(N-1)
+    for i=1:N-1
+      du4[i] = kTd(uguess[i])*(h/12.)*(uguess[i]+uguess[i+1])
+    end
+
+    dm4 = zeros(N)
+    dm4[1] = -9. # it gets cut away so w/e
+    for i=2:N-1
+      dm4[i] = kTd(uguess[i])*(uguess[i-1]*h/12. + uguess[i]*h/2. + uguess[i+1]*h/12.)
+    end
+    dm4[end] = kTd(uguess[end])*(uguess[end-1]*h/12. + uguess[end]*h/4.)
+
+    K7 = diagm(dl4,-1) + diagm(dm4, 0) + diagm(du4, 1)
+    K7 = K7[2:end, :]
+    K7 = -a*b*K7
+
+    B1 = (K3 + K4 + K6)*uguess + (1./dt)*M*(uguess - us[:,tloop-1])
+
+    K5 = K5[:, 2:end]
+    K7 = K7[:, 2:end]
+    # du0 = 0 because we know u0 = 1 = u
+
+    KK = 0.5*(K1 + K2 + K5 + K7) - M[:,2:end]*(1./dt)
+    du = \(KK, B1)
+    uguess = uguess + alpha*[0., du]
+
+    (abs(maximum(du)) < 1.E-05 || counter > maxIter) ? newtonFlag = false : newtonFlag = true
+    counter += 1
+  end
+
+  us[:,tloop] = uguess
+
+end
+
+mesh(xs, ts, us')
+xlabel("Space")
+ylabel("Time")
+
+# plot(xs, us[:, end])
+
+plt.show()
